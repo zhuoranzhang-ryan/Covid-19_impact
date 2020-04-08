0f2e72946cb51f84ce66aca218d77a2ac20241e2
nqazimn <nqazimn@gmail.com>
2020-04-07 22:25:36 -0500

    added prelim code to identify states if they exist

--- static/js/bar_chart.js
+++ static/js/bar_chart.js
@@ -13,20 +13,30 @@ function initializeDashboard() {
 
 function sumStateCases(records) {
     var states = [];
-    var total_cases = [];
+    var total_cases_per_state = [];
+    var sum = 0;
 
     records.forEach(function (record) {
-        if (states.includes(record.Province)) {
-            // console.log("SEEN");
+        if (~(states.includes(record.Province))) {
+            // total_cases.push(sum);
+            states.push(record.Province);
+            total_cases_per_state = []
+            total_cases_per_state.push(record.Cases);
+            // sum = record.Cases;
         }
         else {
-            states.push(record.Province);
+            total_cases_per_state.push(record.Cases);
         }
         // else if (~(record.Province in states)) {
         //     print(record.Province in states);
         // }
     })
-    console.log(states);
+    if (total_cases_per_state.length > 0) {
+        var sum = total_cases_per_state.reduce(function (a, b) {
+            return a + b;
+        }, 0);
+        console.log(sum);
+    }
 
 }
 
