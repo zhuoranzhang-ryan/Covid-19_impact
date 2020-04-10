49b091ef32867808e9add0dea3fb0d9737c2441c
nqazimn <nqazimn@gmail.com>
2020-04-09 21:03:09 -0500

    Summed total cases per state

--- static/js/bar_chart.js
+++ static/js/bar_chart.js
@@ -4,44 +4,83 @@ function initializeDashboard() {
     d3.json(confirmedCases).then(function (dataSet) {
         console.log(dataSet);
 
+        var totalDays = Object.keys(dataSet).length;
+
+        // for (let idx = 0; idx < totalDays; idx++) {
+        //     var dateString = Object.keys(dataSet)[idx];
+        //     var records = Object.values(dataSet)[idx]
+        //     var statesWithTotalCasesDict = {}
+
+        //     var statesWithTotalCases = sumCasesPerState(records);
+
+        //     statesWithTotalCasesDict[dateString] = statesWithTotalCases;
+
+        //     console.log(statesWithTotalCasesDict);
+
+        //     console.log("\n");
+        // }
+
         Object.entries(dataSet).forEach(function ([date, records]) {
-            console.log(`${date}:${records.length}`)
-            sumStateCases(records);
+            var statesWithTotalCasesDict = {}
+
+            var statesWithTotalCases = sumCasesPerState(records);
+
+            statesWithTotalCasesDict[date] = statesWithTotalCases;
+
+            console.log(statesWithTotalCasesDict);
+
         });
     })
 }
 
-function sumStateCases(records) {
-    var states = [];
-    var total_cases_per_state = [];
-    var sum = 0;
+function sumCasesPerState(array) {
+    var states = ['Alabama', 'Alaska', 'American Samoa',
+        'Arizona', 'Arkansas', 'California', 'Colorado',
+        'Connecticut', 'Delaware', 'District of Columbia',
+        'Federated States of Micronesia', 'Florida', 'Georgia',
+        'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+        'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands',
+        'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi',
+        'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey',
+        'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+        'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau',
+        'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota',
+        'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington',
+        'West Virginia', 'Wisconsin', 'Wyoming']; //source: https://gist.github.com/tleen/6299431
 
-    records.forEach(function (record) {
-        if (~(states.includes(record.Province))) {
-            // total_cases.push(sum);
-            states.push(record.Province);
-            total_cases_per_state = []
-            total_cases_per_state.push(record.Cases);
-            // sum = record.Cases;
+    // Initialize totalCasesPerState with 0
+
+    // Copy array and set it to zero
+    var totalCasesPerState = [...states];
+    totalCasesPerState.fill(0);
+
+    for (let idx = 0; idx < array.length; idx++) {
+        // console.log(`${array[idx].Province}: ${array[idx].Cases}`);
+
+        var stateIndex = states.indexOf(array[idx].Province);
+
+        if (stateIndex > -1) {
+            totalCasesPerState[stateIndex] = totalCasesPerState[stateIndex] + array[idx].Cases;
         }
-        else {
-            total_cases_per_state.push(record.Cases);
-        }
-        // else if (~(record.Province in states)) {
-        //     print(record.Province in states);
-        // }
-    })
-    if (total_cases_per_state.length > 0) {
-        var sum = total_cases_per_state.reduce(function (a, b) {
-            return a + b;
-        }, 0);
-        console.log(sum);
     }
+
+    // Create dictionary of states and cases for a date
+    var statesWithTotalCases = {}
+
+    for (let i = 0; i < totalCasesPerState.length; i++) {
+        statesWithTotalCases[states[i]] = totalCasesPerState[i];
+    }
+
+    return statesWithTotalCases;
 
 }
 
 function createBarChart(covidData) {
     // console.log(covidData);
+    var xValues = covidData;
+    var yValues = OTUdata.otu_ids.slice(0, 11);
+    var OTULabels = OTUdata.otu_labels.slice(0, 11);
+
 
 }
 initializeDashboard();
