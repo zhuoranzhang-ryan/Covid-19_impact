ada9d6e86d6253d60554234cf090caf122ae4669
nqazimn <nqazimn@gmail.com>
2020-04-09 22:33:29 -0500

    sorted and sliced covid dictionary to plot bar chart

--- static/js/bar_chart.js
+++ static/js/bar_chart.js
@@ -6,20 +6,41 @@ function initializeDashboard() {
 
         var totalDays = Object.keys(dataSet).length;
 
-        Object.entries(dataSet).forEach(function ([date, records]) {
+        for (let idx = 70; idx < 71; idx++) {
+            var dateString = Object.keys(dataSet)[idx];
+            var records = Object.values(dataSet)[idx]
             var statesWithTotalCasesDict = {}
 
             var statesWithTotalCases = sumCasesPerState(records);
 
-            statesWithTotalCasesDict[date] = statesWithTotalCases;
+            statesWithTotalCasesDict[dateString] = statesWithTotalCases;
 
             console.log(statesWithTotalCasesDict);
 
-        });
+            console.log("\n");
+
+            createBarChart(dateString, statesWithTotalCases);
+        }
+
+        // Object.entries(dataSet).forEach(function ([date, records]) {
+        //     var statesWithTotalCasesDict = {}
+
+        //     var statesWithTotalCases = sumCasesPerState(records);
+
+        //     statesWithTotalCasesDict[date] = statesWithTotalCases;
+
+        //     console.log(statesWithTotalCasesDict);
+
+        //     // createBarChart(date, statesWithTotalCases);
+        //     createBarChart(date, statesWithTotalCases);
+
+        // });
+
     })
 }
 
 function sumCasesPerState(array) {
+    //source: https://gist.github.com/tleen/6299431
     var states = ['Alabama', 'Alaska', 'American Samoa',
         'Arizona', 'Arkansas', 'California', 'Colorado',
         'Connecticut', 'Delaware', 'District of Columbia',
@@ -32,12 +53,12 @@ function sumCasesPerState(array) {
         'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau',
         'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota',
         'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington',
-        'West Virginia', 'Wisconsin', 'Wyoming']; //source: https://gist.github.com/tleen/6299431
+        'West Virginia', 'Wisconsin', 'Wyoming'];
 
     // Initialize totalCasesPerState with 0
 
     // Copy array and set it to zero
-    var totalCasesPerState = [...states];
+    var totalCasesPerState = [...states]; // APPARENTLY, this is how you copy an array in Javascript!
     totalCasesPerState.fill(0);
 
     for (let idx = 0; idx < array.length; idx++) {
@@ -61,12 +82,95 @@ function sumCasesPerState(array) {
 
 }
 
-function createBarChart(covidData) {
+function createBarChart(dateString, covidData) {
     // console.log(covidData);
-    var xValues = covidData;
-    var yValues = OTUdata.otu_ids.slice(0, 11);
-    var OTULabels = OTUdata.otu_labels.slice(0, 11);
+
+    sortedCovidData = (sort_object(covidData));
+
+    var xValues = [];
+    var yValues = [];
+
+    // Populate x and y values
+    Object.entries(sortedCovidData).forEach(function ([state, cases]) {
+        xValues.push(state);
+        yValues.push(cases);
+    });
+
+    console.log(xValues);
+    console.log(yValues);
+
+    // console.log(Math.max.apply(Math, yValues));
 
 
+    var trace = {
+        x: yValues.slice(0, 16),
+        y: xValues.slice(0, 16),
+        type: "bar",
+        orientation: "h",
+        name: "confirmedCases",
+        // text: labels,
+        marker: {
+            color: 'rgba(50,171,96,0.6)',
+            line: {
+                color: 'rgba(50,171,96,1.0)',
+                width: 0
+            }
+        },
+        hovertemplate:
+            "Confirmed: %{x}<br>" +
+            "<extra></extra>"
+    }
+    var layout = {
+        title: `Top 15 states with confirmed cases on ${dateString}`,
+        margin: {
+            l: 120,
+            r: 20,
+            t: 30,
+            b: 40
+        },
+        yaxis: {
+            autorange: "reversed",
+            ticks: "outside",
+            ticklen: 10,
+            tickcolor: "white",
+            linecolor: 'gray',
+            linewidth: 0.01,
+            mirror: true
+        },
+        xaxis: {
+            // tickformat: ',.0%',
+            range: [0, Math.max.apply(Math, yValues)],
+            // side: "top",
+            tickmode: "linear",
+            tick0: 0,
+            dtick: Math.round(Math.max.apply(Math, yValues) / 5),
+            border: 1,
+            linecolor: 'gray',
+            linewidth: 0.5,
+            mirror: true
+        }
+    };
+
+    var data = [trace];
+
+    Plotly.newPlot("stacked_bar_chart", data, layout, { displayModeBar: false });
 }
+// source: https://bit.ly/3bYZ2NY 
+function sort_object(obj) {
+    var items = Object.keys(obj).map(function (key) {
+        return [key, obj[key]];
+    });
+    items.sort(function (first, second) {
+        return second[1] - first[1];
+    });
+    // console.log(items[0]);
+
+    var sorted_obj = {}
+
+    for (let idx = 0; idx < items.length; idx++) {
+        sorted_obj[items[idx][0]] = items[idx][1];
+    }
+    return (sorted_obj)
+}
+
 initializeDashboard();
