a890bd16de6d42a0bd6f25987a853c396d01ccf9
Ryan Zhang <zhan3230@umn.edu>
2020-04-11 11:19:39 -0500

    adding play button functionality

--- static/js/covid-map.js
+++ static/js/covid-map.js
@@ -28,6 +28,7 @@ async function getData(url) {
 var slider = document.getElementById("myRange");
 var output = document.getElementById("value");
 output.innerHTML = slider.value;
+var button = document.getElementById("play-button");
 
 getData(mapfile).then(mapdata => {
 
@@ -73,26 +74,47 @@ getData(mapfile).then(mapdata => {
             var coords = projection([d.Lon, d.Lat])
             if (coords) {return coords[1];}
         });
-        var track_date;    
+
+        let track_date = 0;
         slider.oninput = function() {
-            track_date = this.value;
-            output.innerHTML = this.value;
+            track_date = +this.value;
             let index = this.value;
             let data = Object.values(response)[index];
-            drawCircleLayer(confirmLayer, data, "covid-confirmed", projection)
-            // console.log(track_date);          
-        };
-        console.log(slider.value);
-        // playSlider(track_date, response, projection);
+            let dates = Object.keys(response);  
+            output.innerHTML = dates[index];
 
-        // playSlider(11, response, projection);
+            drawCircleLayer(confirmLayer, data, "covid-confirmed", projection)
+            console.log(track_date);          
+        };
+        
+        var play_flag = false;
+        button.addEventListener("click", function() {
+            track_date = +slider.value;
+            if (!play_flag) {
+                play_flag = true;
+                console.log("It is playing, change text to pause");
+                console.log("Current date:", play_flag);
+                button.innerHTML = 'pause';
+                timer = setInterval(() => {
+                    moveSlider(track_date, response, projection);
+                    // console.log(track_date);
+                    track_date += 1;
+            }, 100);
+            } else if (play_flag) {
+                play_flag = false;
+                console.log("It is not playing, change text to play");
+                button.innerHTML = 'play'
+                clearInterval(timer);
+            }
+        })
 });
 })
 
 // colorScale function
 var colorScale = d3.scaleLinear()
     .domain(d3.ticks(0, 50000, 500))
-    .range([ "#FF9999", "#FF6666", "#FF3333", "#FF0000"]);
+    // .range([ "#FF9999", "#FF6666", "#FF3333", "#FF0000"]);
+    .range(["green", "yellow", "red"]);
 
 function drawCircleLayer(layer, layerdata, layerClass, projection) {
     layer.html('');
@@ -100,7 +122,8 @@ function drawCircleLayer(layer, layerdata, layerClass, projection) {
         .data(layerdata)
         .enter().append('circle')
         .attr("class", layerClass)
-        .attr("r", d => Math.log2(d.Cases))
+        .attr("r", d => Math.log2(d.Cases) + 1)
+        // .attr("r", d => {retutn (Math.log2(d.Cases)+2)})
         .attr("cx", d => {
             var coords = projection([d.Lon, d.Lat]);
             if (coords) {return coords[0];}
@@ -118,27 +141,15 @@ slider.addEventListener("input", function() {
     slider.style.background = color;
 });
 
-async function playSlider(startday, response, projection) {
-    // var startday = await function() {
-    //     if (!track_date) {
-    //         console.log(track_date);
-    //         return track_date;
-    //     }
-    // };
-    setInterval(() => {
-        moveSlider(startday, response, projection);
-        startday += 1;
-    }, 1000);  
-}
-
-async function moveSlider(input, response, projection) {
+function moveSlider(input, response, projection) {
     slider.value = input;
-    output.innerHTML = slider.value;
     let index = slider.value;
     console.log(index);
     // console.log(response);
     let data = Object.values(response)[index];
-    // let dates = Object.keys(response);    
+    let dates = Object.keys(response);  
+    output.innerHTML = dates[index];
+  
     d3.selectAll(".layers").html('');
     let confirmLayer = d3.select(".layers")
     drawCircleLayer(confirmLayer, data, "covid-confirmed", projection)
