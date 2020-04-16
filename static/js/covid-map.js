6e3a1dcac5a162355925dd487ea4ce1296c20ace
Connor Roemelt <57331300+croemelt@users.noreply.github.com>
2020-04-15 19:54:59 -0500

    c code

--- static/js/covid-map.js
+++ static/js/covid-map.js
@@ -18,6 +18,7 @@ var svg = d3.select("#US-map")
 
 const mapfile = "static/data/us.json";
 const covid_confirm_file = "static/data/covid_us_confirmed.json";
+const states_file = "static/data/states_coords.csv";
 
 async function getData(url) {
     let response = await fetch(url);
@@ -30,6 +31,8 @@ var output = document.getElementById("value");
 output.innerHTML = slider.value;
 
 getData(mapfile).then(mapdata => {
+
+d3.csv(states_file).then(data => {
 
     var projection = d3.geoAlbersUsa()
                        .translate([svgWidth/2, svgHeight/2])
@@ -53,40 +56,109 @@ getData(mapfile).then(mapdata => {
         .attr("class", "state")
         .attr("d", path)
 
-    getData(covid_confirm_file).then(response => {
 
-        var data = Object.values(response)[0];
-        var slider_max = Object.keys(response).length;
-        d3.select(".slider").attr("max", slider_max-1);
+    var g = svg.append('g').attr('class', 'marker_layer');
 
-        var confirmLayer = d3.select(".map-svg").append('g').attr('class', "layers")
-        confirmLayer.selectAll(".covid-confirmed")
+    var div = d3.select("#US-map").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+
+    // console.log(typeof(parseFloat(data[18].latitude)))
+
+    // console.log(data)
+    var nodes = g.selectAll('g.node')
         .data(data)
-        .enter().append("circle")
-        .attr("class", "covid-confirmed")
-        .attr("r", d => d.Cases)
-        .attr("cx", d => {
-            var coords = projection([d.Lon, d.Lat]);
-            if (coords) {return coords[0];}
-        })
-        .attr("cy", d => {
-            var coords = projection([d.Lon, d.Lat])
-            if (coords) {return coords[1];}
-        });
-        var track_date;    
-        slider.oninput = function() {
-            track_date = this.value;
-            output.innerHTML = this.value;
-            let index = this.value;
-            let data = Object.values(response)[index];
-            drawCircleLayer(confirmLayer, data, "covid-confirmed", projection)
-            // console.log(track_date);          
-        };
-        console.log(slider.value);
-        // playSlider(track_date, response, projection);
+        .enter().append('g').attr('class', 'node')
+        .attr('transform', function(d) { 
+            coordinates = projection([parseFloat(d.longitude), parseFloat(d.latitude)])
+            if (coordinates) {
+                return 'translate(' + (coordinates[0]-12) + ',' + (coordinates[1]-30) + ')';
 
-        // playSlider(11, response, projection);
-});
+            }
+         
+        })
+        
+        // New
+        .on("mouseover", function(d) {
+            // console.log('hi')
+            div.transition()
+                .duration(200)
+                // .text(function(d) {return "hi"})
+                .style("opacity", 9);
+            div	.html(d.state)	
+                .style("left", (d3.event.pageX) + "px")		
+                .style("top", (d3.event.pageY - 28) + "px")	
+        })
+        .on("mouseout", function(d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", 0)
+          })  
+        
+        // Drawing the icons
+        nodes.append('path')
+        .attr('d', 'M16,0C9.382,0,4,5.316,4,12.001c0,7,6.001,14.161,10.376,19.194   C14.392,31.215,15.094,32,15.962,32c0.002,0,0.073,0,0.077,0c0.867,0,1.57-0.785,1.586-0.805   c4.377-5.033,10.377-12.193,10.377-19.194C28.002,5.316,22.619,0,16,0z M16.117,29.883c-0.021,0.02-0.082,0.064-0.135,0.098   c-0.01-0.027-0.084-0.086-0.129-0.133C12.188,25.631,6,18.514,6,12.001C6,6.487,10.487,2,16,2c5.516,0,10.002,4.487,10.002,10.002   C26.002,18.514,19.814,25.631,16.117,29.883z')
+        // .attr('fillrule', 'evenodd')
+        // .attr('cliprule', 'evenodd')
+        .attr('fill', '#333333')
+        .attr("stroke-width", 100000);
+        nodes.append('path')
+        .attr('d', 'M16.002,17.746c3.309,0,6-2.692,6-6s-2.691-6-6-6   c-3.309,0-6,2.691-6,6S12.693,17.746,16.002,17.746z M16.002,6.746c2.758,0,5,2.242,5,5s-2.242,5-5,5c-2.758,0-5-2.242-5-5   S13.244,6.746,16.002,6.746z')
+        // .attr('fillrule', 'evenodd')
+        // .attr('cliprule', 'evenodd')
+        .attr('fill', '#333333')
+        // .on("mouseover", console.log('ok'))
+        // .on("mouseout", console.log('out'));
+
+        g.selectAll('path')
+        .data(topojson.feature(mapdata, mapdata.objects.states).features)
+        .enter()
+        .append('path')
+        .attr('d', path);
+        
+    });
+
+    
+
+
+
+
+      
+//     getData(covid_confirm_file).then(response => {
+
+//         var data = Object.values(response)[0];
+//         var slider_max = Object.keys(response).length;
+//         d3.select(".slider").attr("max", slider_max-1);
+
+//         var confirmLayer = d3.select(".map-svg").append('g').attr('class', "layers")
+//         confirmLayer.selectAll(".covid-confirmed")
+//         .data(data)
+//         .enter().append("circle")
+//         .attr("class", "covid-confirmed")
+//         .attr("r", d => d.Cases)
+//         .attr("cx", d => {
+//             var coords = projection([d.Lon, d.Lat]);
+//             console.log(coords)
+//             if (coords) {return coords[0];}
+//         })
+//         .attr("cy", d => {
+//             var coords = projection([d.Lon, d.Lat])
+//             if (coords) {return coords[1];}
+//         });
+//         var track_date;    
+//         slider.oninput = function() {
+//             track_date = this.value;
+//             output.innerHTML = this.value;
+//             let index = this.value;
+//             let data = Object.values(response)[index];
+//             drawCircleLayer(confirmLayer, data, "covid-confirmed", projection)
+//             // console.log(track_date);          
+//         };
+//         console.log(slider.value);
+//         playSlider(track_date, response, projection);
+
+//         playSlider(11, response, projection);
+// });
 })
 
 // colorScale function
