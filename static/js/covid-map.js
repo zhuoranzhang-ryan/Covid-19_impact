8e1ab20766d5be77166dcdc412650f4825925799
Ryan Zhang <zhan3230@umn.edu>
2020-04-17 19:49:39 -0500

    almost there

--- static/js/covid-map.js
+++ static/js/covid-map.js
@@ -213,83 +213,181 @@ getData(mapfile).then((mapdata) => {
                 clearInterval(timer);
               }
 
-              // creating the map
-              var projection = d3
-                .geoAlbersUsa()
-                .translate([svgWidth / 2, svgHeight / 2])
-                .scale(900);
+              // // creating the map
+              // var projection = d3
+              //   .geoAlbersUsa()
+              //   .translate([svgWidth / 2, svgHeight / 2])
+              //   .scale(900);
 
-              var path = d3.geoPath().projection(projection);
+              // var path = d3.geoPath().projection(projection);
 
-              marker_layer.addEventListener("change", function () {
-                if (this.checked) {
-                  //tooltip
-                  var g = svg.append("g").attr("class", "marker_layer");
-
-                  var div = d3
-                    .select("#US-map")
-                    .append("div")
-                    .attr("class", "tooltip")
-                    .style("opacity", 0);
-
-                  var nodes = g
-                    .selectAll("g.node")
-                    .data(tooltip_data)
-                    .enter()
-                    .append("g")
-                    .attr("class", "node")
-                    .attr("transform", function (d) {
-                      coordinates = projection([
-                        parseFloat(d.longitude),
-                        parseFloat(d.latitude),
-                      ]);
-                      if (coordinates) {
-                        return (
-                          "translate(" +
-                          (coordinates[0] - 12) +
-                          "," +
-                          (coordinates[1] - 30) +
-                          ")"
-                        );
-                      }
-                    })
-                    .on("mouseover", function (d) {
-                      div.transition().duration(200).style("opacity", 9);
-                      div
-                        .html(d.state)
-                        .style("left", d3.event.pageX + "px")
-                        .style("top", d3.event.pageY - (svgHeight/3) + "px");
-                    })
-                    .on("mouseout", function (d) {
-                      div.transition().duration(200).style("opacity", 0);
-                    });
-
-                  // Drawing the icons
-                  nodes
-                    .append("path")
-                    .attr(
-                      "d",
-                      "M16,0C9.382,0,4,5.316,4,12.001c0,7,6.001,14.161,10.376,19.194   C14.392,31.215,15.094,32,15.962,32c0.002,0,0.073,0,0.077,0c0.867,0,1.57-0.785,1.586-0.805   c4.377-5.033,10.377-12.193,10.377-19.194C28.002,5.316,22.619,0,16,0z M16.117,29.883c-0.021,0.02-0.082,0.064-0.135,0.098   c-0.01-0.027-0.084-0.086-0.129-0.133C12.188,25.631,6,18.514,6,12.001C6,6.487,10.487,2,16,2c5.516,0,10.002,4.487,10.002,10.002   C26.002,18.514,19.814,25.631,16.117,29.883z"
-                    )
-                    .attr("fillrule", "evenodd")
-                    .attr("cliprule", "evenodd")
-                    .attr("fill", "#333333")
-                    .attr("stroke-width", 100000);
-                  nodes
-                    .append("path")
-                    .attr(
-                      "d",
-                      "M16.002,17.746c3.309,0,6-2.692,6-6s-2.691-6-6-6   c-3.309,0-6,2.691-6,6S12.693,17.746,16.002,17.746z M16.002,6.746c2.758,0,5,2.242,5,5s-2.242,5-5,5c-2.758,0-5-2.242-5-5   S13.244,6.746,16.002,6.746z"
-                    )
-                    .attr("fillrule", "evenodd")
-                    .attr("cliprule", "evenodd")
-                    .attr("fill", "#333333");
-                } else {
-                  d3.selectAll(".marker_layer").remove();
-                }
-              });
+              
               // gg.call(zoom);
             });
+            marker_layer.addEventListener("change", function () {
+              if (this.checked) {
+                //tooltip
+                var g = svg.append("g").attr("class", "marker_layer");
+
+                var div = d3
+                  .select("#US-map")
+                  .append("div")
+                  .attr("class", "tooltip")
+                  .style("opacity", 0);
+
+                var nodes = g
+                  .selectAll("g.node")
+                  .data(tooltip_data)
+                  .enter()
+                  .append("g")
+                  .attr("class", "node")
+                  .attr("transform", function (d) {
+                    coordinates = projection([
+                      parseFloat(d.longitude),
+                      parseFloat(d.latitude),
+                    ]);
+                    if (coordinates) {
+                      return (
+                        "translate(" +
+                        (coordinates[0] - 12) +
+                        "," +
+                        (coordinates[1] - 30) +
+                        ")"
+                      );
+                    }
+                  })
+                  .on("mouseover", function (d) {
+                    div.transition().duration(200).style("opacity", 9);
+                    div
+                      .html(d.state)
+                      .style("left", d3.event.pageX + "px")
+                      .style("top", d3.event.pageY - (svgHeight/3) + "px");
+                  })
+                  .on("mouseout", function (d) {
+                    div.transition().duration(200).style("opacity", 0);
+                  });
+
+                // Drawing the icons
+                nodes
+                  .append("path")
+                  .attr(
+                    "d",
+                    "M16,0C9.382,0,4,5.316,4,12.001c0,7,6.001,14.161,10.376,19.194   C14.392,31.215,15.094,32,15.962,32c0.002,0,0.073,0,0.077,0c0.867,0,1.57-0.785,1.586-0.805   c4.377-5.033,10.377-12.193,10.377-19.194C28.002,5.316,22.619,0,16,0z M16.117,29.883c-0.021,0.02-0.082,0.064-0.135,0.098   c-0.01-0.027-0.084-0.086-0.129-0.133C12.188,25.631,6,18.514,6,12.001C6,6.487,10.487,2,16,2c5.516,0,10.002,4.487,10.002,10.002   C26.002,18.514,19.814,25.631,16.117,29.883z"
+                  )
+                  .attr("fillrule", "evenodd")
+                  .attr("cliprule", "evenodd")
+                  .attr("fill", "#333333")
+                  .attr("stroke-width", 100000);
+                nodes
+                  .append("path")
+                  .attr(
+                    "d",
+                    "M16.002,17.746c3.309,0,6-2.692,6-6s-2.691-6-6-6   c-3.309,0-6,2.691-6,6S12.693,17.746,16.002,17.746z M16.002,6.746c2.758,0,5,2.242,5,5s-2.242,5-5,5c-2.758,0-5-2.242-5-5   S13.244,6.746,16.002,6.746z"
+                  )
+                  .attr("fillrule", "evenodd")
+                  .attr("cliprule", "evenodd")
+                  .attr("fill", "#333333");
+              } else {
+                d3.selectAll(".marker_layer").remove();
+              }
+            });
+            var zoomSettings = {
+              duration: 1000,
+              ease: d3.easeCubicOut,
+              zoomLevel: 5,
+            };
+            
+            function clicked(d) {
+              var x;
+              var y;
+              var zoomLevel;
+              var centered;
+            
+              if (d && centered !== d) {
+                var centroid = path.centroid(d);
+                x = centroid[0];
+                y = centroid[1];
+                zoomLevel = zoomSettings.zoomLevel;
+                centered = d;
+              } else {
+                x = width / 2;
+                y = height / 2;
+                zoomLevel = 1;
+                centered = null;
+              }
+              counties_map
+                .transition()
+                .duration(zoomSettings.duration)
+                .ease(zoomSettings.ease)
+                .attr(
+                  "transform",
+                  "translate(" +
+                    width / 2 +
+                    "," +
+                    height / 2 +
+                    ")scale(" +
+                    zoomLevel +
+                    ")translate(" +
+                    -x +
+                    "," +
+                    -y +
+                    ")"
+                );
+              states_map
+                .transition()
+                .duration(zoomSettings.duration)
+                .ease(zoomSettings.ease)
+                .attr(
+                  "transform",
+                  "translate(" +
+                    width / 2 +
+                    "," +
+                    height / 2 +
+                    ")scale(" +
+                    zoomLevel +
+                    ")translate(" +
+                    -x +
+                    "," +
+                    -y +
+                    ")"
+                );
+              confirmLayer
+                .transition()
+                .duration(zoomSettings.duration)
+                .ease(zoomSettings.ease)
+                .attr(
+                  "transform",
+                  "translate(" +
+                    width / 2 +
+                    "," +
+                    height / 2 +
+                    ")scale(" +
+                    zoomLevel +
+                    ")translate(" +
+                    -x +
+                    "," +
+                    -y +
+                    ")"
+                );
+              d3.selectAll(".marker_layer")
+                .transition()
+                .duration(zoomSettings.duration)
+                .ease(zoomSettings.ease)
+                .attr(
+                  "transform",
+                  "translate(" +
+                    width / 2 +
+                    "," +
+                    height / 2 +
+                    ")scale(" +
+                    zoomLevel +
+                    ")translate(" +
+                    -x +
+                    "," +
+                    -y +
+                    ")"
+                );
+            }
           });
         });
       });
@@ -429,100 +527,4 @@ function initializeMarker(tooltip_data, state_sum_data, projection) {
     .attr("fill", "#333333");
 }
 
-var zoomSettings = {
-  duration: 1000,
-  ease: d3.easeCubicOut,
-  zoomLevel: 5,
-};
 
-function clicked(d) {
-  var x;
-  var y;
-  var zoomLevel;
-  var centered;
-
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    x = centroid[0];
-    y = centroid[1];
-    zoomLevel = zoomSettings.zoomLevel;
-    centered = d;
-  } else {
-    x = width / 2;
-    y = height / 2;
-    zoomLevel = 1;
-    centered = null;
-  }
-  counties_map
-    .transition()
-    .duration(zoomSettings.duration)
-    .ease(zoomSettings.ease)
-    .attr(
-      "transform",
-      "translate(" +
-        width / 2 +
-        "," +
-        height / 2 +
-        ")scale(" +
-        zoomLevel +
-        ")translate(" +
-        -x +
-        "," +
-        -y +
-        ")"
-    );
-  states_map
-    .transition()
-    .duration(zoomSettings.duration)
-    .ease(zoomSettings.ease)
-    .attr(
-      "transform",
-      "translate(" +
-        width / 2 +
-        "," +
-        height / 2 +
-        ")scale(" +
-        zoomLevel +
-        ")translate(" +
-        -x +
-        "," +
-        -y +
-        ")"
-    );
-  confirmLayer
-    .transition()
-    .duration(zoomSettings.duration)
-    .ease(zoomSettings.ease)
-    .attr(
-      "transform",
-      "translate(" +
-        width / 2 +
-        "," +
-        height / 2 +
-        ")scale(" +
-        zoomLevel +
-        ")translate(" +
-        -x +
-        "," +
-        -y +
-        ")"
-    );
-  d3.selectAll(".marker_layer")
-    .transition()
-    .duration(zoomSettings.duration)
-    .ease(zoomSettings.ease)
-    .attr(
-      "transform",
-      "translate(" +
-        width / 2 +
-        "," +
-        height / 2 +
-        ")scale(" +
-        zoomLevel +
-        ")translate(" +
-        -x +
-        "," +
-        -y +
-        ")"
-    );
-}
